478a03a0060dc604ef5c636a90fa953e52ead53d
Sean Temple <110708900+SMTemple@users.noreply.github.com>
2023-03-16 14:24:59 -0400

    Creating main dev dir
    Create dev dir

---
+++ sources/php/db.config.php
@@ -0,0 +1,19 @@
+<?
+	$db['host'] = "localhost";
+	$db['user'] = "temple_user";
+	$db['pass'] = "Cw4hA-9RZgf)";
+	$db['db'] = "temple_sean_db";
+
+	
+	// $get_db = function(&$db) {
+	// 	$file = dirname(__DIR__, 2).'/config.ini';
+
+	// 	if (file_exists($file)) {
+	// 		$config = parse_ini_file($file, true);
+
+	// 		if ($config && isset($config['database']))
+	// 			$db = array_merge($db, $config['database']);
+	// 	}
+	// };
+
+	// $get_db($db);
